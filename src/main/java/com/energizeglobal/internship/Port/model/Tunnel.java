185ba4b4f6d7ba631272ff66c99d85c566028f4a
Arman Arshakyan <armanar@energizeglobal.com>
2020-07-02 13:45:10 +0400

    Tunnel entering implementation switched to Semaphore.

--- src/main/java/com/energizeglobal/internship/Port/model/Tunnel.java
+++ src/main/java/com/energizeglobal/internship/Port/model/Tunnel.java
@@ -4,6 +4,7 @@ import com.energizeglobal.internship.Port.model.ship.Ship;
 import org.apache.log4j.Logger;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -11,26 +12,22 @@ public class Tunnel {
     private static final Logger log = Logger.getLogger(Tunnel.class);
     private static final int SHIP_CAPACITY = 5;
     private volatile AtomicInteger shipCount = new AtomicInteger(0);
+    private Semaphore semaphore = new Semaphore(SHIP_CAPACITY, true);
 
     public void enter(Ship ship) {
         log.info(ship + " tries to enter to " + this + ": " + LocalDateTime.now());
-        while (!this.canEnter()) {
-            try {
-                TimeUnit.SECONDS.sleep(1);
-            } catch (InterruptedException e) {
-                log.error("Thead " + Thread.currentThread().getName() + Thread.currentThread().getId() +
-                        " Interrupted \n " +
-                        "Exception: " + e);
-                throw new RuntimeException("Thread interrupted");
-            }
+        try {
+            semaphore.acquire();
+            goThrough(ship);
+            semaphore.release();
+        } catch (InterruptedException exception) {
+            String threadName = Thread.currentThread().getName();
+            String message = exception.getMessage();
+            log.error("Interrupted exception occurred in thread " + threadName + ": " + message);
         }
-        goThrough(ship);
+
     }
 
-    private synchronized boolean canEnter() {
-        log.info(this + " : " + LocalDateTime.now());
-        return this.shipCount.get() < SHIP_CAPACITY;
-    }
 
     private void goThrough(Ship ship) {
         this.shipCount.incrementAndGet();
